dea79afa4e10125c6e6d9e42adf07ca1495ed6a0
giautm <giau.tran@tiki.vn>
2018-04-23 00:14:34 +0700

    Added isTikier

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -11,7 +11,21 @@ const userPermissions = (user) =>
     return perms;
   }, rolePermissions(user));
 
+const TIKIER_GROUP_NAME = 'TIKI';
+
 const User = {
+  isTikier({ id }, args, ctx, info) {
+    const groupWhere = { name: TIKIER_GROUP_NAME };
+    return ctx.db.exists.User({
+      id,
+      groups_some: {
+        OR: [
+          groupWhere,
+          { parent: groupWhere },
+        ]
+      }
+    });
+  },
   async permissions({ id }, args, ctx, info) {
     const role = `roles{permissions}`;
     const user = await ctx.db.query.user({
